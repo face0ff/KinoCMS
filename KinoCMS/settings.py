aaef3f009b7e4e76d444d8b8419ce0ff13f320bc
face0ff <facekrakadil@gmail.com>
2023-01-28 23:48:35 +0200

    Bugfix final

--- KinoCMS/settings.py
+++ KinoCMS/settings.py
@@ -21,8 +21,8 @@ env = environ.Env(DEBUG=(bool, False))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-# DEBUG = env('DEBUG')
-DEBUG =True
+DEBUG = env('DEBUG')
+# DEBUG =True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -69,8 +69,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'KinoCMS.urls'
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'adminLte/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'adminLte/static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
