575c6b64d9b3c1d510fe34c7b875104f79289c41
face0ff <facekrakadil@gmail.com>
2023-01-29 19:21:37 +0200

    Bugfix final

--- users/management/commands/session_create.py
+++ users/management/commands/session_create.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 
 from django.core.management.base import BaseCommand
 from faker import Faker
@@ -13,22 +14,12 @@ class Command(BaseCommand):
         fake = Faker('ru_RU')
         film_id = Film.objects.all()
         hall_id = Hall.objects.all()
-        for i in range(1, 50):
 
-            Session.objects.create(id=i, name=i, date=fake.date_this_month(after_today=True),
-                                   dateTime=fake.time(pattern='%H:%M:%S'),film=random.choice(film_id),
+
+
+        for i in range(1, 10):
+            for f in film_id:
+                Session.objects.create(name=i, date=(f.release_date + timedelta(days= (random.randint(0, 6)))),
+                                   dateTime=fake.time(pattern='%H:%M:%S'), film=f,
                                    hall=random.choice(hall_id), t2d=True, t3d=False,
                                    tIMAX=False)
-        for i in range(50, 100):
-
-            Session.objects.create(id=i, name=i, date=fake.date_this_month(after_today=True),
-                                   dateTime=fake.time(pattern='%H:%M:%S'),film=random.choice(film_id),
-                                   hall=random.choice(hall_id), t2d=False, t3d=True,
-                                   tIMAX=False)
-
-        for i in range(100, 150):
-
-            Session.objects.create(id=i, name=i, date=fake.date_this_month(after_today=True),
-                                   dateTime=fake.time(pattern='%H:%M:%S'),film=random.choice(film_id),
-                                   hall=random.choice(hall_id), t2d=False, t3d=False,
-                                   tIMAX=True)
